63c29abbec2e85b963fec25250aba7cae0f6aac7
Carlos Perez <solracias@gmail.com>
2017-06-22 14:34:37 -0700

    server is set up

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 var express = require("express");
 var override = require("method-override");
 var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+
+// view route require
+// api route require
+
+app.listen(PORT, function(){
+    console.log("app listening on PORT " + PORT);
+})
